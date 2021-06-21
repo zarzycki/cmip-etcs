4c3d56c598def78b3b74b2bb4ef05f250625f7d9
Colin Zarzycki <zarzycki@ucar.edu>
2021-06-21 12:31:41 -0600

    add renaming to combine_comps file

--- for-gmd-v2/combine-comps.sh
+++ for-gmd-v2/combine-comps.sh
@@ -11,3 +11,8 @@ ncks -A composite_PSL.nc composite_CESM1-LENS.nc
 ncks -A composite_TREFHT.nc composite_CESM1-LENS.nc
 ncks -A composite_QREFHT.nc composite_CESM1-LENS.nc
 ncks -A -v hyai,hybi,hyam,hybm,lev,ilev /glade/u/home/zarzycki/betacast/atm_to_cam/templates/L30template.nc composite_CESM1-LENS.nc 
+ncrename -v U\(:\),U composite_CESM1-LENS.nc
+ncrename -v V\(:\),V composite_CESM1-LENS.nc
+ncrename -v T\(:\),T composite_CESM1-LENS.nc
+ncrename -v Q\(:\),Q composite_CESM1-LENS.nc
+
